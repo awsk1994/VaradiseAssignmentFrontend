f42978324ca7383f64c273fb2d835028ad9fa204
awsk1994 <awsk1994@gmail.com>
2023-09-12 01:17:27 +0800

    init structure for frontend

--- src/App.js
+++ src/App.js
@@ -1,23 +1,17 @@
 import logo from './logo.svg';
 import './App.css';
+import { Route, Routes, Redirect, Navigate } from 'react-router-dom';
+import Main from './Building/Main';
+import Login from './Login/Login';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Routes>
+        <Route exact path="/" element={<Navigate to="/buildings" replace/>}/>
+        <Route path="/login" element={<Login />} />
+        <Route path="/buildings" element={<Main />} />
+      </Routes>
     </div>
   );
 }
---
+++ src/Building/Main.jsx
@@ -0,0 +1,16 @@
+import Overview from "./Overview";
+import Chart from "./Chart";
+
+function Main() {
+    return (
+        <div>
+            <h1>Main</h1>
+
+            <Overview/>
+            <Chart/>
+            
+        </div>
+    )
+}
+
+export default Main;
